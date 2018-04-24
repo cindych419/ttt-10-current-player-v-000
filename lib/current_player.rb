3d80d7a16efc3cfc851621059b85e124fd2172f1
cindych419@gmail.com <cindych419@gmail.com>
2018-04-24 01:17:07 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,2 +1,8 @@
-def turn_count
-  
+def turn_count(board)
+  counter = 0
+  board = ["X", "O", "X", "O", "X", "O", "X", "O", "X"]
+  board.each do |inside|
+  puts "#{counter}"
+  puts "#{inside}"
+  counter +=1
+
